fbab34e510656539f6f5cd68d0358c4a7e49243c
Vivek Gupta <>
2018-05-14 13:46:11 +0530

    Shadow on section header added

--- DynamicUIDemo/TwoWayTableView/RightTableHeaderView.swift
+++ DynamicUIDemo/TwoWayTableView/RightTableHeaderView.swift
@@ -13,6 +13,7 @@ class RightTableHeaderView: UIView {
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet var contentView: UIView!
     var array: [String] = []
+    let shadowView = UIView()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -38,6 +39,21 @@ class RightTableHeaderView: UIView {
         layout.minimumInteritemSpacing = 0.0
         collectionView.setCollectionViewLayout(layout, animated: false)
         collectionView.dataSource = self
+        shadowView.frame = CGRect(x: 0, y: self.frame.height - 2, width: self.frame.width, height: 1)
+        self.contentView.addSubview(shadowView)
+        self.applyBottomShadowToSelfView()
+    }
+    
+    func applyBottomShadowToSelfView(){
+        let contentInset = UIEdgeInsetsMake(0, 0, 0, 0)
+        let shadowPathOnlyRight = UIEdgeInsetsInsetRect(CGRect(x: 0, y: 0, width: shadowView.frame.width, height: 2), contentInset)
+        let path = UIBezierPath.init(rect: shadowPathOnlyRight)
+        shadowView.layer.shadowPath = path.cgPath
+        shadowView.layer.shadowColor = UIColor.black.cgColor
+        shadowView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        shadowView.layer.shadowOpacity = 0.3
+        shadowView.layer.shadowRadius = 3.0
+        shadowView.layer.masksToBounds = false
     }
 }
 extension RightTableHeaderView: UICollectionViewDataSource{
